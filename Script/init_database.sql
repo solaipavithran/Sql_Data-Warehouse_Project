c0881ef64ad9696baf22c0d492db08f48fde19d5
solai pavithran.s <60392569+solaipavithran@users.noreply.github.com>
2025-03-17 19:38:15 +0530

    Update init_database.sql

--- Script/init_database.sql
+++ Script/init_database.sql
@@ -1,5 +1,21 @@
-CREATE DATABASE DataWarehouse;
-USE DataWarehouse;
+/*
+------------------------------------------------------
+Create Database and Schemas
+-------------------------------------------------------
+
+Script Purpose:
+This script creates a new database named 'DataWarehouse' after checking if it already exists.
+Additionally, the script sets up three schemas
+within the database: 'bronze', 'silver', and 'gold'.
+
+*/
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'DataWarehouse')
+    BEGIN
+        CREATE DATABASE DataWarehouse;
+    END;
+
+    USE DataWarehouse;
 
 
 CREATE SCHEMA bronze;
